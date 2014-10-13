167c76ad792a7bbb9bdcc6eac4fb79b21ca3ec28
AZ technology <aztec@localhost.domain>
2014-10-13 18:39:22 +0800

    Initial import from sdk4.4_r1
    origin/master@40e5e57
    
    Change-Id: I1276d0253e942abb67cffef8030f83825b540612

---
+++ prebuild.mk
@@ -0,0 +1,6 @@
+#GENERATE MANIFEST
+$_rbox_$_modify_$_huangyonglin
+#$(shell test -d .repo && .repo/repo/repo manifest -r -o manifest.xml)
+$_rbox_$_modify_$_end
+-include $(TARGET_DEVICE_DIR)/prebuild.mk
+
