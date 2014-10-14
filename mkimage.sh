67d661a438ec28ab7baaae384e8b21987d40b103
AZ technology <aztec@localhost.domain>
2014-10-14 16:57:01 +0800

    mkimage.sh: adjust misc images

--- mkimage.sh
+++ mkimage.sh
@@ -77,8 +77,8 @@ fi
 
 	echo -n "create misc.img.... "
 	cp -a rkst/Image/misc.img $IMAGE_PATH/misc.img
-	cp -a rkst/Image/pcba_small_misc.img $IMAGE_PATH/pcba_small_misc.img
-	cp -a rkst/Image/pcba_whole_misc.img $IMAGE_PATH/pcba_whole_misc.img
+	cp -a rkst/Image/misc_wipe_all.img $IMAGE_PATH/misc_wipe_all.img
+	cp -a rkst/Image/misc_factory_test.img $IMAGE_PATH/misc_factory_test.img
 	echo "done."
 
 if [ -d $OUT/system ]
