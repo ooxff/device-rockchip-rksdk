30e836335bd35adabba58182c70728167f757cf1
AZ technology <aztec@localhost.domain>
2014-12-08 19:10:36 +0800

    Add GaiaConfig.mk
    1. diable ftu
    2. enable debug
    3. disable screen lock

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -53,6 +53,8 @@ BOARD_FLASH_BLOCK_SIZE ?= 131072
 include device/rockchip/$(TARGET_PRODUCT)/wifi_bt.mk
 include device/rockchip/rksdk/wifi_bt_common.mk
 
+include device/rockchip/rksdk/GaiaConfig.mk
+
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 
---
+++ GaiaConfig.mk
@@ -0,0 +1,7 @@
+# one-file-make does not need export, such as Android.mk
+GAIA_DEVICE_TYPE=phone
+
+# sub-make needs export, such as gaia/Makefile
+export NOFTU=1
+export DEVICE_DEBUG=1
+export NO_LOCK_SCREEN=1
